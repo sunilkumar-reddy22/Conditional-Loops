33c61d6309b211e529d236ca2fba959a0adec048
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-15 00:22:59 +0530

    Added logical operator

--- src/Loops/CommonNumbers.java
+++ src/Loops/CommonNumbers.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class CommonNumbers {
     public static void main(String[] args) {
 
-        System.out.println("enter a integer: ");
+        Scanner input = new Scanner(System.in);
  //       int num = 23454;
        while(true) {
-           Scanner input = new Scanner(System.in);
+           System.out.println("enter a integer (-1 to stop): ");
            int num = input.nextInt();
            int count = 0;
            while (num > 0) {
                int rem = num % 10;
-               if (rem == 4) {
+               if (rem == 4 || rem == 1 || rem == 2 || rem == 3 ) {
                    count++;
                }
                num = num / 10;
