9b238ba5605a6e0f83cd7706af35629079b8f915
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-15 00:53:55 +0530

    changed variable name

--- src/Loops/ReverseInteger.java
+++ src/Loops/ReverseInteger.java
@@ -4,13 +4,13 @@ public class ReverseInteger {
     public static void main(String[] args) {
         System.out.println("give values mate: ");
         int num =56478;
-        int ans = 0;
+        int result = 0;
         while(num > 0) {
             int rem = num % 10;
             num = num / 10;
 
-            ans = ans * 10 + rem;
+            result = result * 10 + rem;
         }
-        System.out.println(ans);
+        System.out.println(result);
     }
 }
