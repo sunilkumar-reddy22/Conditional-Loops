e953276a6df88d6db19a0d21af7d95b9cf13740b
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-15 22:27:00 +0530

    add number utility

--- src/Loops/Countintegerpractice.java
+++ src/Loops/Countintegerpractice.java
@@ -4,14 +4,27 @@ public class Countintegerpractice {
     public static void main(String[] args) {
         long num = 234456;
         int count = 0;
-        while(num > 0) {
-             long  rem = num % 10;
+        while (num > 0) {
+            long rem = num % 10;
             if (rem == 4) {
                 count++;
             }
             num = num / 10;
         }
         System.out.println(count);
-    }
 
+        System.out.println("\nReverse number: ");
+        long num1 = 74684;
+        long  ans = 0;
+        while (num1 > 0) {
+            long  rem1 = num1 % 10;
+            ans = ans * 10 + rem1;
+            num1 = num1 / 10;
+
+        }
+        System.out.println(ans);
+    }
 }
+
+
+
